f389f35c59ee1b0ffc5bd250461041f25ff3f796
Luigi Indaco <luigindaco@gmail.com>
2022-10-26 16:50:34 +0200

    consegna

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,30 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", function () { });
+class Lavoratore {
+    constructor(codeRedd, redditoAnnuoLordo, tasseInps, tasseIrpef) {
+        this.codeRedd = codeRedd;
+        this.redditoAnnuoLordo = redditoAnnuoLordo;
+        this.tasseInps = tasseInps;
+        this.tasseIrpef = tasseIrpef;
+    }
+    getUtileTasse() {
+        return (this.redditoAnnuoLordo * (this.tasseInps + this.tasseIrpef)) / 100;
+    }
+    getTasseInps() {
+        return (this.redditoAnnuoLordo -= (this.redditoAnnuoLordo * this.tasseInps) / 100);
+    }
+    getTasseIrpef() {
+        return (this.redditoAnnuoLordo -= (this.redditoAnnuoLordo * this.tasseIrpef) / 100);
+    }
+    getRedditoAnnuoNetto() {
+        let valoreTasse = this.getUtileTasse();
+        return (this.redditoAnnuoLordo -= valoreTasse);
+    }
+}
+class LavoratoreAutonomo extends Lavoratore {
+}
+let lavoratore1 = new LavoratoreAutonomo(1, 100, 15, 10);
+// console.log(lavoratore1.getUtileTasse());
+// console.log(lavoratore1.getTasseInps());
+// console.log(lavoratore1.getTasseIrpef());
+console.log(lavoratore1.getRedditoAnnuoNetto());
---
+++ ts/scipt.ts
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {});
+
+abstract class Lavoratore {
+  private codeRedd: number; //codice lavoratore
+  private redditoAnnuoLordo: number; // valore reddito lordo
+  private tasseInps: number; // detrazione tasse inps da reddito
+  private tasseIrpef: number; // detrazione tasse irpef da reddito
+
+  constructor(codeRedd: number, redditoAnnuoLordo: number, tasseInps: number, tasseIrpef: number) {
+    this.codeRedd = codeRedd;
+    this.redditoAnnuoLordo = redditoAnnuoLordo;
+    this.tasseInps = tasseInps;
+    this.tasseIrpef = tasseIrpef;
+  }
+  getUtileTasse() {
+    return (this.redditoAnnuoLordo * (this.tasseInps + this.tasseIrpef)) / 100;
+  }
+  getTasseInps() {
+    return (this.redditoAnnuoLordo -= (this.redditoAnnuoLordo * this.tasseInps) / 100);
+  }
+  getTasseIrpef() {
+    return (this.redditoAnnuoLordo -= (this.redditoAnnuoLordo * this.tasseIrpef) / 100);
+  }
+  getRedditoAnnuoNetto() {
+    let valoreTasse = this.getUtileTasse();
+    return (this.redditoAnnuoLordo -= valoreTasse);
+  }
+}
+
+class LavoratoreAutonomo extends Lavoratore {}
+let lavoratore1 = new LavoratoreAutonomo(1, 100, 15, 10);
+
+// console.log(lavoratore1.getUtileTasse());
+// console.log(lavoratore1.getTasseInps());
+// console.log(lavoratore1.getTasseIrpef());
+console.log(lavoratore1.getRedditoAnnuoNetto());
